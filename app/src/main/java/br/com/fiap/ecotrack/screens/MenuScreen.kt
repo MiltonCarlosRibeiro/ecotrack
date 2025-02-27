b203e870532751e8c4c5aaa958b8a7c0cab12d34
Milton-C-Ribeiro <milton.carlos.java@gmail.com>
2025-02-27 00:03:09 -0300

    First commit Ecotrack

---
+++ app/src/main/java/br/com/fiap/ecotrack/screens/MenuScreen.kt
@@ -0,0 +1,109 @@
+package br.com.fiap.ecotrack.screens
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MenuScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF2C4EC7))
+            .padding(32.dp)
+    ) {
+        Text(
+            text = "MENU",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.Center)
+        ) {
+
+
+            Button(
+                onClick = {/*TODO*/ },
+                colors = ButtonDefaults.buttonColors(Color.White),
+                modifier = Modifier.size(width = 200.dp, height = 48.dp)
+
+            ) {
+                Text(text = "Editar Perfil", fontSize = 20.sp, color = Color.Blue)
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {/*TODO*/ },
+                colors = ButtonDefaults.buttonColors(Color.White),
+                modifier = Modifier.size(width = 200.dp, height = 48.dp)
+
+            ) {
+                Text(text = "Monitoramento", fontSize = 20.sp, color = Color.Blue)
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {/*TODO*/ },
+                colors = ButtonDefaults.buttonColors(Color.White),
+                modifier = Modifier.size(width = 200.dp, height = 48.dp)
+
+            ) {
+                Text(text = "Desafios", fontSize = 20.sp, color = Color.Blue)
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {/*TODO*/ },
+                colors = ButtonDefaults.buttonColors(Color.White),
+                modifier = Modifier.size(width = 200.dp, height = 48.dp)
+
+            ) {
+                Text(text = "Relatórios", fontSize = 20.sp, color = Color.Blue)
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {/*TODO*/ },
+                colors = ButtonDefaults.buttonColors(Color.White),
+                modifier = Modifier.size(width = 200.dp, height = 48.dp)
+
+            ) {
+                Text(text = "Recompensas", fontSize = 20.sp, color = Color.Blue)
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+        }
+
+
+
+    }
+
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview(){
+    MenuScreen()
+}
+
+
